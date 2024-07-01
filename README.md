0ed6788b4fa7b30ccd97f16a133391469e183003
Zoe1976 <pazomelts@gmail.com>
2024-07-01 20:35:00 +1000

    Add files via upload
    I have added a home page and changed the images and style of the original game as well as fixed some broken links

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# Memeory-Game
-memeory game using HTML, CSS and basic JAVASCRIPT
+# Memory-Game
+Simple emory game with easy and hard modes, using HTML, CSS and basic JAVASCRIPT
 
 HTML--
-The HTML (Hypertext Markup Language) code will help us to prepare a game board and the necessary elements to create the structure.
+The HTML (Hypertext Markup Language) code will help us to explain the game and prepare a game board and the necessary elements to create the structure.
 
 CSS--
-The CSS (Cascading Stylesheet) code will help to style the project with suitable bgcolor and some alignment to the necessary elements.
+The CSS (Cascading Stylesheet) code will help to style the project with suitable background colour and some alignment to the necessary elements.
 
 JAVASCRPIT--
 Then the JS(JavaScript) code will contain the images of the game and then we’ll make it a grid and some functions to make it responsive at the user end.
 
-I hope you have got an idea about the project.
+Great game for stimulating your Brain!!
--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var cardsArray = [
-    {    'name': 'CSS',    'img': 'https://github.com/robgmerrill/img/blob/master/css3-logo.png?raw=true',  },
-    {    'name': 'HTML',    'img': 'https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true',  },
-    {    'name': 'jQuery',    'img': 'https://github.com/robgmerrill/img/blob/master/jquery-logo.png?raw=true',  },
-    {    'name': 'JS',    'img': 'https://github.com/robgmerrill/img/blob/master/js-logo.png?raw=true',  },
-    {    'name': 'Node',    'img': 'https://github.com/robgmerrill/img/blob/master/nodejs-logo.png?raw=true',  },
-    {    'name': 'Photo Shop',    'img': 'https://github.com/robgmerrill/img/blob/master/photoshop-logo.png?raw=true',  },
-    {    'name': 'PHP',    'img': 'https://github.com/robgmerrill/img/blob/master/php-logo_1.png?raw=true',  },
-    {    'name': 'Python',    'img': 'https://github.com/robgmerrill/img/blob/master/python-logo.png?raw=true',  },
-    {    'name': 'Ruby',    'img': 'https://github.com/robgmerrill/img/blob/master/rails-logo.png?raw=true',  },
-    {    'name': 'Sass',    'img': 'https://github.com/robgmerrill/img/blob/master/sass-logo.png?raw=true',  },
-    {    'name': 'Sublime',    'img': 'https://github.com/robgmerrill/img/blob/master/sublime-logo.png?raw=true',  },
-    {    'name': 'Wordpress',    'img': 'https://github.com/robgmerrill/img/blob/master/wordpress-logo.png?raw=true',  },
+    {    'name': 'donkey',    'img': 'img/donkey.png',  },
+    {    'name': 'cat',    'img': 'img/cat.png',  },
+    {    'name': 'bird',    'img': 'img/bird.png',  },
+    {    'name': 'cow',    'img': 'img/cow.png',  },
+    {    'name': 'dog',    'img': 'img/dog.png',  },
+    {    'name': 'elephant',    'img':'img/elephant.png',  },
+    {    'name': 'penguin',    'img': 'img/penguin.png',  },
+    {    'name': 'pig',    'img': 'img/pig.jpg',  },
+    {    'name': 'turtle',    'img': 'img/turtle.png',  },
+    {    'name': 'fish',    'img':'img/fish.jpg',  },
+    {    'name': 'giraffe',    'img': 'img/giraffe.jpg',  },
+    {    'name': 'frog',    'img': 'img/frog.png',  },
   ];
 
 //Duplicate cardsArray to create a match for each card
@@ -131,6 +131,8 @@ grid.addEventListener('click', function(event) {
         }
     }
     previousTarget = clicked;
+
+    
     }
 
 });
